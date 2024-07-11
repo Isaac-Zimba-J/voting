40044eb1ebeec5b51ebf6950fa37ed0ec8ea5b29
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2024-07-11 14:32:23 +0200

    GHOST MIDDLEWARE

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware'
+    #'django.contrib.staticfiles.middleware.StaticFilesMiddleware'
 
     'account.middleware.AccountCheckMiddleWare',
 ]
