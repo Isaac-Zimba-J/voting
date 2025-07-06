e51313813e262d1ae2a820db071f2d5b29837308
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2025-07-06 20:34:32 +0200

    new bash

--- build.sh
+++ build.sh
@@ -3,26 +3,15 @@
 
 set -o errexit
 
-
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files and run migrations
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-if[[ $CREATE_SUPERUSER == "true" ]]; 
-then
-    python manage.py createsuperuser --no-input  --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME" 
-fiA#!/usr/bin/env bash
-# This script builds the Docker image for the project.
-
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
-python manage.py migrate
-
-if [ "$CREATE_SUPERUSER" = "true" ]; then
+# Optional: Create superuser if environment variable is set
+if [[ "$CREATE_SUPERUSER" == "true" ]]; then
   python manage.py createsuperuser \
     --no-input \
     --email "$DJANGO_SUPERUSER_EMAIL" \
