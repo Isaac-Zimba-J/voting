86f81d5386f4fb22c4cb5bffeeb0653193f75a96
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2025-07-06 22:56:19 +0200

    harrdcode super user

--- build.sh
+++ build.sh
@@ -17,3 +17,21 @@ if [[ "$CREATE_SUPERUSER" == "true" ]]; then
     --email "$DJANGO_SUPERUSER_EMAIL" \
     --username "$DJANGO_SUPERUSER_USERNAME"
 fi
+
+
+# Hardcode superuser creation
+echo "Creating hardcoded superuser..."
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = 'admin'
+email = '21111111@gmail.com'
+password = 'Admin@1234'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+"
