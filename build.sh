447a1f2a5f526dc5f4efe24bf4283549595ebd49
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2025-07-06 18:15:40 +0200

    chore: Add build script for Docker image creation

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This script builds the Docker image for the project.
+
+set -o errexit
+
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+if[[ $CREATE_SUPERUSER == "true" ]]; 
+then
+    python manage.py createsuperuser --no-input  --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME" 
+fi
