5b05ba05f35e8123d3492ead1d1162b087396412
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2025-07-06 20:15:59 +0200

    fix: Correct syntax in build script and update gunicorn version in requirements.txt

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-if[[ $CREATE_SUPERUSER == "true" ]]; 
+if [[ $CREATE_SUPERUSER == "true" ]]; 
 then
     python manage.py createsuperuser --no-input  --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME" 
 fiA#!/usr/bin/env bash
